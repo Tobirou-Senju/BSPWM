1d5609791ef3edab9390ebcaf134f5a24154a58c
Tobirou-Senju <a9998964@gmail.com>
2025-02-28 11:28:55 +0530

    Update install_dependencies.sh

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,30 +1,34 @@
 #!/bin/bash
 
-# Ensure the script is run with sudo or as root
-if [ "$(id -u)" -ne 0 ]; then
-    echo "This script must be run as root or with sudo."
-    exit 1
+# Ensure the script is run with sudo
+if [[ $EUID -ne 0 ]]; then
+   echo "Please run this script as root (use sudo)." 
+   exit 1
 fi
 
+# Update package lists
 echo "Updating package lists..."
-apt update
+apt update -y
 
-echo "Installing required dependencies..."
+# Install the specified packages
+echo "Installing required packages..."
 apt install -y \
-    build-essential cmake meson ninja-build git wget curl \
-    libconfig-dev libdbus-1-dev libegl-dev libev-dev libgl-dev \
-    libepoxy-dev libpcre2-dev libpixman-1-dev libx11-xcb-dev \
-    libxcb1-dev libxcb-composite0-dev libxcb-damage0-dev \
-    libxcb-dpms0-dev libxcb-glx0-dev libxcb-image0-dev \
-    libxcb-present-dev libxcb-randr0-dev libxcb-render0-dev \
-    libxcb-render-util0-dev libxcb-shape0-dev libxcb-util-dev \
-    libxcb-xfixes0-dev libxext-dev uthash-dev \
-    libgtk-4-dev libadwaita-1-dev \
-    pkg-config
+    xorg xbacklight xbindkeys xvkbd xinput build-essential bspwm sxhkd polybar \
+    network-manager network-manager-gnome pamixer thunar thunar-archive-plugin \
+    thunar-volman file-roller lxappearance dialog mtools dosfstools avahi-daemon \
+    acpi acpid gvfs-backends xfce4-power-manager pavucontrol pamixer pulsemixer \
+    feh fonts-recommended fonts-font-awesome fonts-terminus ttf-mscorefonts-installer \
+    papirus-icon-theme exa flameshot qimgv rofi dunst libnotify-bin xdotool unzip \
+    libnotify-dev geany geany-plugin-addons geany-plugin-git-changebar \
+    geany-plugin-spellcheck geany-plugin-treebrowser geany-plugin-markdown \
+    geany-plugin-insertnum geany-plugin-lineoperations geany-plugin-automark \
+    pipewire-audio \
+    nala micro xdg-user-dirs-gtk tilix \
+    --install-recommends arctica-greeter
 
-# Check if the installation was successful
-if [ $? -eq 0 ]; then
-    echo "Installation completed successfully!"
+# Check if installation was successful
+if [[ $? -eq 0 ]]; then
+    echo "All packages installed successfully!"
 else
-    echo "Installation encountered errors. Check the output for details."
+    echo "Some packages failed to install. Please check for errors."
 fi
